2418c0f63d0622a134c37fd187648197ed928097
LiangChaokai <youchoudeyu@qq.com>
2018-10-02 15:29:05 +0800

    iview

--- src/main.js
+++ src/main.js
@@ -2,6 +2,28 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// import Vue from 'vue'
+import { Header, Footer, Row, Col, Content, Sider, Divider, Menu, MenuItem, Icon, Layout, Carousel, CarouselItem } from 'iview'
+
+// import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+
+import './plugins/iview.js'
+// Vue.use(iView)
+Vue.component('Header', Header)
+Vue.component('Footer', Footer)
+Vue.component('Content', Content)
+Vue.component('Row', Row)
+Vue.component('Col', Col)
+Vue.component('Sider', Sider)
+Vue.component('Divider', Divider)
+Vue.component('Menu', Menu)
+Vue.component('MenuItem', MenuItem)
+Vue.component('Icon', Icon)
+Vue.component('Layout', Layout)
+Vue.component('Carousel', Carousel)
+Vue.component('CarouselItem', CarouselItem)
+
 Vue.config.productionTip = false
 
 new Vue({
